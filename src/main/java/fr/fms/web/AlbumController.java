4842d2f11fce34b818b1ceb23937c43c72bb17bf
Aurore Chevin <chevinaurore@gmail.com>
2023-05-24 09:05:30 +0200

    controller album create, albums.html create, display table albums in view

--- src/main/java/fr/fms/web/AlbumController.java
+++ src/main/java/fr/fms/web/AlbumController.java
@@ -1,16 +1,22 @@
 package fr.fms.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import fr.fms.dao.AlbumRepository;
+import fr.fms.entities.Album;
 @Controller
 public class AlbumController {
 	@Autowired
 	AlbumRepository albumRepository;
 	@GetMapping("/index")
-	public String index() {
+	public String index(Model model) {
+		List<Album> albums = albumRepository.findAll();
+		model.addAttribute("listAlbums", albums);
 		return "albums";
 	}
 }
