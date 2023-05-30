bf7da13fe234a392d55ea60d2d3b5bf92e8694c4
Aurore Chevin <chevinaurore@gmail.com>
2023-05-30 13:46:19 +0200

    display albums by musicGenre and pagination

--- src/main/java/fr/fms/dao/AlbumRepository.java
+++ src/main/java/fr/fms/dao/AlbumRepository.java
@@ -2,14 +2,22 @@ package fr.fms.dao;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import fr.fms.entities.Album;
 
 public interface AlbumRepository extends JpaRepository<Album, Long>{
 	
 	public Page<Album> findByAlbumNameContains(String albumName, Pageable pageable);
-	public List<Album> findByMusicGenreId(Long musicGenreId);
+	public Page<Album> findByMusicGenreId(Long musicGenreId, Pageable pageable);
+	@Transactional
+	@Modifying
+	@Query("update Album a set a.albumName = ?2 , a.bandName = ?3, a.releaseYear = ?4, a.price = ?5 where a.id = ?1")
+	public int updateAlbumById(Long id, String albumName, String bandName, int releaseYear, double price);
 }
--- src/main/java/fr/fms/web/AlbumController.java
+++ src/main/java/fr/fms/web/AlbumController.java
@@ -7,12 +7,10 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,8 +18,6 @@ import fr.fms.dao.AlbumRepository;
 import fr.fms.dao.MusicGenreRepository;
 import fr.fms.entities.Album;
 import fr.fms.entities.MusicGenre;
-import lombok.extern.slf4j.Slf4j;
-@Slf4j
 @Controller
 public class AlbumController {
 	@Autowired
@@ -30,13 +26,19 @@ public class AlbumController {
 	MusicGenreRepository musicGenreRepository;
 	@GetMapping("/index")
 	public String index(Model model, @RequestParam(name="page", defaultValue = "0") int page,
-									@RequestParam(name="keyword", defaultValue = "") String kw
-									) {
-		Page<Album> albums = albumRepository.findByAlbumNameContains(kw, PageRequest.of(page, 5));
+									@RequestParam(name="keyword", defaultValue = "") String kw,
+									@RequestParam(name="musicGenreId", defaultValue = "0") Long musicGenreId) {
 		List<MusicGenre> musicGenres = musicGenreRepository.findAll();
+		Page<Album> albums;
+		if(musicGenreId==0)
+			albums = albumRepository.findByAlbumNameContains(kw, PageRequest.of(page, 5));
+		else {
+			albums = albumRepository.findByMusicGenreId(musicGenreId, PageRequest.of(page, 5));
+		}
 		model.addAttribute("listMusicGenres", musicGenres);
 		model.addAttribute("listAlbums", albums.getContent());
 		model.addAttribute("pages", new int[albums.getTotalPages()]);
+		model.addAttribute("musicGenreId", musicGenreId);
 		model.addAttribute("currentPage", page);
 		model.addAttribute("keyword", kw);
 		return "albums";
@@ -44,7 +46,7 @@ public class AlbumController {
 	@GetMapping("/delete")
 	public String delete(Long id, int page, String keyword) {
 		albumRepository.deleteById(id);
-		log.info("Supprimé");
+		System.out.println("Supprimé");
 		return "redirect:/index?page="+page+"&keyword="+keyword;
 	}
 	@GetMapping("/album")
@@ -56,18 +58,9 @@ public class AlbumController {
 	public String save(Model model, @Valid Album album, BindingResult bindingResult) {
 		if(bindingResult.hasErrors()) return "album";
 		albumRepository.save(album);
-		log.info("Enregistré");
+		System.out.println("Enregistré");
 		return "redirect:/index";
 	}
 	
-	@GetMapping("/{musicGenreId}")
-	public String getAlbumsByMusicGenre(@PathVariable Long musicGenreId, Model model) {
-		List<MusicGenre> musicGenres = musicGenreRepository.findAll();
-		model.addAttribute("listMusicGenres", musicGenres);
-		model.addAttribute("listAlbums", albumRepository.findByMusicGenreId(musicGenreId));
-		log.info("ok");
-		System.out.println(musicGenreId);
-		return "albums";
-	}
 	
 }
